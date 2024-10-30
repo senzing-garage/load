3c00afbf9cbb57aa17fdc5a7418815f3f95cab3a
docktermj <michael@dockter.com>
2024-10-30 15:35:40 -0400

    #90 Update dependencies

--- input/rabbitmq/rabbitmq.go
+++ input/rabbitmq/rabbitmq.go
@@ -21,14 +21,21 @@ func Read(ctx context.Context, urlString, engineConfigJSON, logLevel string, jso
 		panic("Cannot set log level")
 	}
 
-	// Work with szEngine.
-	szEngine := createSzEngine(ctx, engineConfigJSON, senzing.SzNoLogging)
+	szAbstractFactory, err := szfactorycreator.CreateCoreAbstractFactory("load", engineConfigJSON, senzing.SzNoLogging, senzing.SzInitializeWithDefaultConfiguration)
+	if err != nil {
+		panic(err)
+	}
 	defer func() {
-		err := szEngine.Destroy(ctx)
+		err := szAbstractFactory.Destroy(ctx)
 		if err != nil {
 			panic(err)
 		}
 	}()
+
+	szEngine, err := szAbstractFactory.CreateSzEngine(ctx)
+	if err != nil {
+		panic(err)
+	}
 
 	startErr := rabbitmq.StartManagedConsumer(ctx, urlString, 0, &szEngine, false, logLevel, jsonOutput)
 
@@ -39,25 +46,6 @@ func Read(ctx context.Context, urlString, engineConfigJSON, logLevel string, jso
 }
 
 // ----------------------------------------------------------------------------
-
-func getAbstractFactory(ctx context.Context, engineConfigJSON string, verboseLogging int64) senzing.SzAbstractFactory {
-	_ = ctx
-	result, err := szfactorycreator.CreateCoreAbstractFactory("load", engineConfigJSON, verboseLogging, senzing.SzInitializeWithDefaultConfiguration)
-	if err != nil {
-		panic(err)
-	}
-	return result
-}
-
-// create a SzEngine object, on error this function panics.
-// see failOnError
-func createSzEngine(ctx context.Context, settings string, verboseLogging int64) senzing.SzEngine {
-	result, err := getAbstractFactory(ctx, settings, verboseLogging).CreateSzEngine(ctx)
-	if err != nil {
-		log(2004, err.Error())
-	}
-	return result
-}
 
 var logger logging.Logging
 var jsonOutput bool
