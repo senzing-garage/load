6e0da5c7edc60db95a98ffd071c2414f10ad5c60
Michael Dockter <michael@dockter.com>
2025-05-13 15:07:51 -0400

    #131 Working lint/test

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -13,41 +13,47 @@ import (
 // ----------------------------------------------------------------------------
 
 func Test_Execute(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	os.Args = []string{"command-name", "--help"}
 
 	cmd.Execute()
 }
 
 func Test_Execute_completion(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	os.Args = []string{"command-name", "completion"}
 
 	cmd.Execute()
 }
 
 func Test_Execute_docs(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	os.Args = []string{"command-name", "docs"}
 
 	cmd.Execute()
 }
 
 func Test_Execute_help(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	os.Args = []string{"command-name", "--help"}
 
 	cmd.Execute()
 }
 
 func Test_PreRun(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	args := []string{"command-name", "--help"}
 	cmd.PreRun(cmd.RootCmd, args)
 }
 
 func Test_completionCmd(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	err := cmd.CompletionCmd.Execute()
 	require.NoError(test, err)
 	err = cmd.CompletionCmd.RunE(cmd.CompletionCmd, []string{})
@@ -55,7 +61,8 @@ func Test_completionCmd(test *testing.T) {
 }
 
 func Test_docsCmd(test *testing.T) {
-	_ = test
+	test.Parallel()
+
 	err := cmd.DocsCmd.Execute()
 	require.NoError(test, err)
 	err = cmd.DocsCmd.RunE(cmd.DocsCmd, []string{})
